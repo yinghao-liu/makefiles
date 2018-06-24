a3ec2e3ae060b42ff38955c68f0815ee4e4532ae
francis <yinghao_liu@163.com>
2018-06-24 22:07:55 +0800

    auxiliary 3rdparty

---
+++ 3rdparty/3rdparty.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int thirdparty_init(void)
+{
+	cout<<"now in thirdparty_init"<<endl;
+	return 0;
+}
---
+++ 3rdparty/3rdparty.h
@@ -0,0 +1,6 @@
+#ifndef _3RDPARTY_
+#define _3RDPARTY_
+
+int thirdparty_init(void);
+
+#endif
