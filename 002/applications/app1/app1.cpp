10db46e329dddd9fe1e030186508323240b45993
francis <yinghao_liu@163.com>
2019-10-12 21:26:58 +0800

    done nearly

--- 002/applications/app1/app1.cpp
+++ 002/applications/app1/app1.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "3rdparty.h"
+#include "ma.h"
 using namespace std;
 int app1_init(void)
 {
-	cout<<"now in app1_init"<<endl;
+	cout<<"-----now in app1_init-----"<<endl;
+	thirdparty_init();
+	ma_init();
+	cout<<"-----end of app1_init-----"<<endl;
 	return 0;
 }
